8d41d2162699b78afe6364bf200dccf23ce58988
berlaschnikov <91128974+berlaschnikov@users.noreply.github.com>
2022-12-13 20:57:08 +0100

    Adding Login Including Link to regpage and app

---
+++ ExibitMeProject/ExibitMeProject/LoginPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExibitMeProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isUsernameEmpty = string.IsNullOrEmpty(UsernameEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+
+            if (isUsernameEmpty)
+            {
+                UsernameEntry.Placeholder = "This can not be empty!";
+            }
+            else if (isPasswordEmpty)
+            {
+                PasswordEntry.Placeholder = "This can not be empty";
+            }
+            else
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+        }
+
+        private void RegisterButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegPage());
+        }
+    }
+}
