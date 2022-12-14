0e3389ce34f13899e199469bf587903b4c488be4
Mitch <mitch@LAPTOP-MITCH>
2022-12-14 19:50:37 +0100

    LoginPage aangemaakt

--- ExibitMeProject/ExibitMeProject/RegPage.xaml.cs
+++ ExibitMeProject/ExibitMeProject/RegPage.xaml.cs
@@ -16,5 +16,24 @@ namespace ExibitMeProject
         {
             InitializeComponent();
         }
+
+        private void RegisterButton_Clicked(object sender, EventArgs e)
+        {
+            bool isUsernameEmpty = string.IsNullOrEmpty(UsernameEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+
+            if (isUsernameEmpty)
+            {
+                UsernameEntry.Placeholder = "This can not be empty!";
+            }
+            else if (isPasswordEmpty)
+            {
+                PasswordEntry.Placeholder = "This can not be empty";
+            }
+            else
+            {
+                Navigation.PushAsync(new LoginPage());
+            }
+        }
     }
 }
