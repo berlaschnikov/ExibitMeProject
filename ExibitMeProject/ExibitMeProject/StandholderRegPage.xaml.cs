6e61774b57ce7b90e4f822e7c5b1fe05d047d5b0
Mitch <mitch@LAPTOP-MITCH>
2022-12-15 12:23:39 +0100

    Per gebruiker registratie pagina aangemaakt.

---
+++ ExibitMeProject/ExibitMeProject/StandholderRegPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExibitMeProject
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StandholderRegPage : ContentPage
+	{
+		public StandholderRegPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void RegisterButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ExibitMeProject/ExibitMeProject/VisitorRegPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExibitMeProject
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VisitorRegPage : ContentPage
+	{
+		public VisitorRegPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void RegisterButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
