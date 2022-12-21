b599a638a26a648149fe2243eb826bf930177444
berlaschnikov <91128974+berlaschnikov@users.noreply.github.com>
2022-12-21 15:08:06 +0100

    Add QrCodeScannerPage fix loginpages

---
+++ ExibitMeProject/ExibitMeProject/QrCodeScannerPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExibitMeProject
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class QrCodeScannerPage : ContentPage
+	{
+		public QrCodeScannerPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
