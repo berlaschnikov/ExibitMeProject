5e0fd37c3752a93a57cef3928467e254ed37cd0e
berlaschnikov <91128974+berlaschnikov@users.noreply.github.com>
2022-12-21 16:13:33 +0100

    standholder

---
+++ ExibitMeProject/ExibitMeProject/VIewTurnoverPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExibitMeProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VIewTurnoverPage : ContentPage
+    {
+        public VIewTurnoverPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
