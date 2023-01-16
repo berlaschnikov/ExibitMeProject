1be1f5d1d90ae1ecda5e286978403f6677955ef8
Mitch <mitch@LAPTOP-MITCH>
2023-01-16 14:02:46 +0100

    Login and register

--- ExibitMeProject/ExibitMeProject/StandholderRegPage.xaml.cs
+++ ExibitMeProject/ExibitMeProject/StandholderRegPage.xaml.cs
@@ -22,9 +22,10 @@ namespace ExibitMeProject
         private void RegisterButton_Clicked(object sender, EventArgs e)
         {
             using SQLiteConnection sQLiteConnection = new SQLiteConnection(App.DatabaseLocation);
-            sQLiteConnection.Insert(new Standholder { Name = NameEntry.Text, Company = CompanyEntry.Text });
+            sQLiteConnection.Insert(new Standholder { Name = NameEntry.Text, Company = CompanyEntry.Text, Password = PasswordEntry.Text });
             Application.Current.Properties["Name"] = NameEntry.Text;
             Application.Current.Properties["Company"] = CompanyEntry.Text;
+            Application.Current.Properties["Password"] = PasswordEntry.Text;
             DisplayAlert("", "Registration Succesful!", "OK");
             Xamarin.Essentials.Vibration.Vibrate(2000);
             Navigation.PushAsync(new StandholderLoginPage());
