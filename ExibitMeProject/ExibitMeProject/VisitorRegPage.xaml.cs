cd3b9121c1e1b079aa6e61ef540be6646adcbcd7
simon <2105134oros@zuyd.nl>
2023-01-16 09:52:34 +0100

    Registerpage Vakje

--- ExibitMeProject/ExibitMeProject/VisitorRegPage.xaml.cs
+++ ExibitMeProject/ExibitMeProject/VisitorRegPage.xaml.cs
@@ -3,6 +3,7 @@ using SQLite;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace ExibitMeProject
 {
@@ -20,6 +22,10 @@ namespace ExibitMeProject
         {
             InitializeComponent();
             this.BindingContext = new VisitorRegPageViewModel();
+
+
+
+
         }
 
         private void RegisterButton_Clicked(object sender, EventArgs e)
@@ -32,7 +38,6 @@ namespace ExibitMeProject
                 sQLiteConnection.Insert(new Visitor { FullName = FullNameEntry.Text, EmailAddress = EmailAddressEntry.Text, Phone = PhoneEntry.Text });
                 Application.Current.Properties["FullName"] = FullNameEntry.Text;
                 Application.Current.Properties["EmailAddress"] = EmailAddressEntry.Text;
-                Application.Current.Properties["Phone"] = PhoneEntry.Text;
                 DisplayAlert("", "Registration Succesful!", "OK");
                 Xamarin.Essentials.Vibration.Vibrate(2000);
                 Navigation.PushAsync(new VisitorLoginPage());
@@ -56,23 +61,72 @@ namespace ExibitMeProject
                 Navigation.PushAsync(new VisitorLoginPage());
             }
         }
-    }
-    public class VisitorRegPageViewModel : INotifyPropertyChanged
-    {
-        private bool _shareData;
-        public bool shareData
+
+        private void ShareDataOption_CheckedChanged(object sender, CheckedChangedEventArgs e)
         {
-            get { return _shareData; }
-            set
+            if (ShareDataOption.IsChecked)
             {
-                _shareData = value;
-                OnPropertyChanged();
+                PostalCode.IsVisible = false;
+                City.IsVisible = false;
+                State.IsVisible = false;
+                Country.IsVisible = false;
+                Street.IsVisible = false;
+                Occupation.IsVisible = false;
+                Phone.IsVisible = false;
+            }
+            else
+            {
+                FullName.IsVisible = true;
+                EmailAddress.IsVisible = true;
+                Phone.IsVisible = true;
+                PostalCode.IsVisible = true;
+                City.IsVisible = true;
+                State.IsVisible = true;
+                Country.IsVisible = true;
+                Street.IsVisible = true;
+                Occupation.IsVisible = true;
             }
         }
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+
+
+        public class VisitorRegPageViewModel : INotifyPropertyChanged
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            private bool _shareData;
+            public bool shareData
+            {
+                get { return _shareData; }
+                set
+                {
+                    _shareData = value;
+                    OnPropertyChanged();
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public class BooleanConverter : IValueConverter, IMarkupExtension
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return !((bool)value);
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return value;
+            }
+
+            public object ProvideValue(IServiceProvider serviceProvider)
+            {
+                return this;
+            }
         }
     }
 }
+
+
+
