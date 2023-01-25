87ca6e37cae59fb48c70a624fb219fc0bcfad481
berlaschnikov <91128974+berlaschnikov@users.noreply.github.com>
2023-01-25 17:39:45 +0100

    u

--- ExibitMeProject/ExibitMeProject/Models/Expo.cs
+++ ExibitMeProject/ExibitMeProject/Models/Expo.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace ExibitMeProject.Models
 {
-    internal class Expo
+    public class Expo
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Date { get; set; }
+        public List<Standholder> Standholders { get; set; }
+        public Organizer Organizer { get; set; }
     }
 }
