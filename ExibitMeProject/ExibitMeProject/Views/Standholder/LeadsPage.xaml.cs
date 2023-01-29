fc6ec6bdd354b85769d2e984dca6539684cbdef9
berlaschnikov <91128974+berlaschnikov@users.noreply.github.com>
2023-01-29 19:30:33 +0100

    fixe lead page

--- ExibitMeProject/ExibitMeProject/Views/Standholder/LeadsPage.xaml.cs
+++ ExibitMeProject/ExibitMeProject/Views/Standholder/LeadsPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExibitMeProject.Services;
+using ExibitMeProject.Models;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +9,41 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Newtonsoft.Json;
 
 namespace ExibitMeProject.Views.Standholder
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LeadsPage : ContentPage
 	{
-		public LeadsPage ()
+        public Models.Standholder UpdatedStandholder { get; set; }
+        public LeadsPage ()
 		{
 			InitializeComponent ();
-		}
+            var currenStandholder = App.CurrentAppStandholder;
+            UpdatedStandholder = currenStandholder;
+            LeadsListView.ItemsSource = UpdatedStandholder.Leads;
+        }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void ScanLeadsButton_Clicked(object sender, EventArgs e)
         {
-
+            var scanner = DependencyService.Get<IQrScanningService>();
+            var result = await scanner.ScanAsync();
+            var info = JsonConvert.DeserializeObject<Models.Visitor>(result);
+            UpdatedStandholder.Leads.Add(info);
+            int updatedRows;
+			try
+			{
+                using (SQLiteConnection sQLiteConnection = new SQLiteConnection(App.DatabaseLocation))
+                {
+                    sQLiteConnection.CreateTable<Models.Standholder>();                    
+                    updatedRows = sQLiteConnection.Update(info);
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
         }
     }
 }
